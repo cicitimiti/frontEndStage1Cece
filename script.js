88e01660a73cbb23d9bb71a884ceb09fb6f3a597
cece <113234020+cicitimiti@users.noreply.github.com>
2023-09-08 04:27:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
-
-function updateClock(){
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    let ampm = "AM";
-
-    if (h > 12) {
-        h = h - 12;
-        ampm ="PM";
-    }
-
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
-
-    hourEl.innerText = h;
-    minuteEl.innerText = m;
-    secondEl.innerText = s;
-    ampmEl, (innerText = ampm);
-    setTimeout(()=>{
-     updateClock()
-    }, 1000)
-}
-
-updateClock()
-
 
 const today = new Date();
 
@@ -37,3 +6,12 @@ const day = today.getDay();
 const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 document.querySelector(".day").innerHTML = dayList[day];
+
+
+function updateUTCTime() {
+    const currentUTCTime = Date.now();
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
+}
+updateUTCTime();
+
+setInterval(updateUTCTime, 1000); 
